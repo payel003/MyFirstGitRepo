7e1077ecb30e91dc0e3e04e543522b706938ad0c
payel <payel003@gmail.com>
2018-01-03 22:13:09 -0500

    New changes done for Jenkins check

--- ProjectForGIT/src/com/project/gitproject/TestGit1.java
+++ ProjectForGIT/src/com/project/gitproject/TestGit1.java
@@ -5,6 +5,7 @@ public class TestGit1 {
 	public static void main(String[] args) {
 
 		System.out.println("This is my latest Project");
+		System.out.println("Now changing to check my Jenkins");
 	}
 
 }
